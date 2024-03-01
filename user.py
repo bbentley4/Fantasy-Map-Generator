3bfa73d060ac9d344319e7a18d354391001111f6
bbentley4 <brennafierro@gmail.com>
2024-03-01 00:13:46 -0500

    cleaned up the python file

--- user.py
+++ user.py
@@ -20,18 +20,17 @@ def main():
     print("2: Desert")
     print("3: Islands")
 
-    landscape = input()
+    landscape = input("\t")
     
+    # ---------------------------------------------------------------------------- #
+    #                               GRASSLAND OPTION                               #
+    # ---------------------------------------------------------------------------- #
     if landscape == '1':
-        output_jgr.write("""newcurve poly color 0.45 0.6 0.41 marktype none linetype solid color 0.45 0.6 0.41
-    pts 0 -70  200 -70  200 70  0 70
-    """)
+        output_jgr.write("newcurve poly color 0.45 0.6 0.41 marktype none linetype solid color 0.45 0.6 0.41\npts 0 -70  200 -70  200 70  0 70")
         
-        print ("Let's start by defining the territories.")
-        
-        print("Next, we'll place the lakes.")
-        print("Enter the coordinate (e.g. E05), as well as the width and height (e.g. 10).")
-        print("When you're done, simply type X when prompted for the coordinate.\n")
+        print("Lakes:")
+        print("Enter the coordinate (e.g. E05), as well as the width and height.")
+        print("When you're done, type X when prompted for the coordinate.\n")
 
         while 1:
             coordinate = input("Coordinate: ")
@@ -54,17 +53,20 @@ def main():
                 continue
             
             width = int(input("Width: "))
-            height = int(input("Height: "))
-            
-            if width > 0 and height > 0:
-                output_jgr.write(f"newcurve eps ./Lake.ps marksize {width} {height} pts {x} {y}\n")
-            else:
-                print("Width and height should be greater than 0.\n")
+            if width < 0:
+                print("Width should be greater than 0.\n")
                 continue
 
-        print("Next, we'll add mountains.")
+            height = int(input("Height: "))
+            if height < 0: 
+                print("Height should be greater than 0.\n")
+                continue
+
+            output_jgr.write(f"newcurve eps ./Lake.ps marksize {width} {height} pts {x} {y}\n")
+
+        print("Mountains:")
         print("Enter the coordinates for the beginning and end of the mountain range.")
-        print("When you're done, simply type X when prompted for the first coordinate.\n")
+        print("When you're done, type X when prompted for the first coordinate.\n")
 
         while 1:
             x1 = None
@@ -120,9 +122,9 @@ def main():
                 output_jgr.write(f"{plot_x} {plot_y}  ")
             output_jgr.write(f"{x2} {y2}\n")
             
-        print("Next, we'll add forests.")
+        print("Forests:")
         print("Enter the coordinate where you'd like a cluster of trees.")
-        print("When you're done, simply type X when prompted for the coordinate.\n")
+        print("When you're done, type X when prompted for the coordinate.\n")
 
         while 1:
             coordinate = input("Coordinate: ")
@@ -145,94 +147,11 @@ def main():
                 print("Invalid coordinate\n")
                 continue
 
-        print("Next, we'll add rivers.")
-        print("Enter the coordinates for the beginning and end of the river and how wide you'd like the river.")
-        print("When you're done, simply type X when prompted for the coordinate.\n")
-
-        while 1:
-            x1 = None
-            x2 = None
-            y1 = None
-            y2 = None
-            width = None
-
-            coordinate1 = input("Coordinate 1: ")
-
-            if coordinate1 == 'X' or coordinate1 == 'x':
-                break
-
-            if len(coordinate1) != 3:
-                print("Invalid coordinate\n")
-                continue
-
-            letter = coordinate1[0].upper()
-            number = coordinate1[1:]
-
-            if letter in letter_values and number in number_values:
-                x1 = letter_values[letter]
-                y1 = number_values[number] 
-            else:
-                print("Invalid coordinate\n")
-                continue
-            
-            coordinate2 = input("Coordinate 2: ")
-
-            if len(coordinate2) != 3:
-                print("Invalid coordinate\n")
-                continue
-
-            letter = coordinate2[0].upper()
-            number = coordinate2[1:]
-
-            if letter in letter_values and number in number_values:
-                x2 = letter_values[letter]
-                y2 = number_values[number] 
-            else:
-                print("Invalid coordinate\n")
-                continue
-
-            width = int(input("Width: "))
-            if (width < 0):
-                print ("Invalid width - must be greater than 0")
-                continue
-        
-            distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
-            #River stretches both ways so I have to plot it at the midpoint
-            midpoint_x = (x1 + x2) / 2
-            midpoint_y = (y1 + y2) / 2
-
-            #Rotate the river to be at the right angle
-            angle = math.degrees(math.atan2(-(y2-y1), x2-x1))
-            river_angle = None
-            if ((x2 - x1) < 0):
-                river_angle = angle + 90
-            if ((x2 - x1) == 0):
-                river_angle = 90
-            else:
-                river_angle = angle + 90
-
-            river_size = 4.5
-            distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-            num_points = int(distance/river_size) + 1
-            add_to_x = ((x2 - x1) / num_points)
-            add_to_y = ((y2 - y1) / num_points)
-
-            for i in range(num_points):
-                plot_x = x1 + i * add_to_x
-                plot_y = y1 + i * add_to_y
-                output_jgr.write(f"newcurve eps ./River.ps marksize {width} 5 mrotate {river_angle} pts {plot_x} {plot_y}\n")
-
-            output_jgr.write(f"newcurve eps ./River.ps marksize {width} 5 mrotate {river_angle} pts {x2} {y2}\n")
-
+    # ---------------------------------------------------------------------------- #
+    #                                 DESERT OPTION                                #
+    # ---------------------------------------------------------------------------- #
     if landscape == '2':
-        output_jgr.write("newcurve poly color 0.93 0.79 0.69 marktype none linetype solid color 0.45 0.6 0.41\n\tpts 0 -70  200 -70  200 70  0 70\n")
-        
-        print ("Let's start by defining the territories.")
-        
-        print("Next, we'll place oases.")
-        print("Enter the coordinate (e.g. E05), as well as the width and height (e.g. 10).")
-        print("When you're done, simply type X when prompted for the coordinate.\n")
+        output_jgr.write("newcurve poly color 0.93 0.79 0.69 marktype none linetype solid color 0.93 0.79 0.69\n\tpts 0 -70  200 -70  200 70  0 70\n")
 
         while 1:
             coordinate = input("Coordinate: ")
@@ -263,7 +182,7 @@ def main():
                 print("Width and height should be greater than 0.\n")
                 continue
 
-        print("Next, we'll add mountains.")
+        print("Mountains:")
         print("Enter the coordinates for the beginning and end of the mountain range.")
         print("When you're done, simply type X when prompted for the first coordinate.\n")
 
@@ -321,7 +240,7 @@ def main():
                 output_jgr.write(f"{plot_x} {plot_y}  ")
             output_jgr.write(f"{x2} {y2}\n")
             
-        print("Next, we'll some cacti.")
+        print("Cacti:")
         print("Enter the coordinate where you'd like a cactus.")
         print("When you're done, simply type X when prompted for the coordinate.\n")
 
@@ -341,12 +260,76 @@ def main():
             if letter in letter_values and number in number_values:
                 x = letter_values[letter]
                 y = number_values[number] 
-                output_jgr.write(f"newcurve eps ./Cactus.ps marksize 15 15 pts {x} {y}\n")
+                output_jgr.write(f"newcurve eps ./Cactus.ps marksize 10 10 pts {x} {y}\n")
             else:
                 print("Invalid coordinate\n")
                 continue
 
+        print("Oases:")
+        print("Enter the coordinate (e.g. E05), as well as the width and height.")
+        print("When you're done, type X when prompted for the coordinate.\n")
 
+        while 1:
+            coordinate = input("Coordinate: ")
+
+            if coordinate == 'X' or coordinate == 'x':
+                break
+
+            if len(coordinate) != 3:
+                print("Invalid coordinate\n")
+                continue
+
+            letter = coordinate[0].upper()
+            number = coordinate[1:]
+
+            if letter in letter_values and number in number_values:
+                x = letter_values[letter]
+                y = number_values[number] 
+            else:
+                print("Invalid coordinate\n")
+                continue
+            
+            width = int(input("Width: "))
+            if width < 0:
+                print("Width should be greater than 0.\n")
+                continue
+
+            height = int(input("Height: "))
+            if height < 0: 
+                print("Height should be greater than 0.\n")
+                continue
+
+            output_jgr.write(f"newcurve eps ./Lake.ps marksize {width} {height} pts {x} {y}\n")
+
+    # ---------------------------------------------------------------------------- #
+    #                                 OCEAN OPTION                                 #
+    # ---------------------------------------------------------------------------- #
+    if landscape == '3':
+        output_jgr.write("newcurve poly color 0.3 0.5 0.7 marktype none linetype solid color 0.3 0.5 0.7\n\tpts 0 -70  200 -70  200 70  0 70\n")
+
+    # ---------------------------------------------------------------------------- #
+    #                                  CLEANING UP                                 #
+    # ---------------------------------------------------------------------------- #
+        
+    # ---------------------------------- Compile --------------------------------- #
+    # Compile Command 1
+    command1 = "jgraph -P map.jgr > map.ps"
+    subprocess.run(command1, shell=True, check=True)
+
+    # Compile Command 2
+    command2_part1 = "jgraph map.jgr"
+    command2_part2 = "convert -density 300 - -quality 100 map.jpg"
+    process1 = subprocess.Popen(command2_part1.split(), stdout=subprocess.PIPE)
+    process2 = subprocess.Popen(command2_part2.split(), stdin=process1.stdout, stdout=subprocess.PIPE)
+    process1.stdout.close()  
+    output, error = process2.communicate()
+
+    # Error Check
+    if error:
+        print("Error in saving map:", error.decode())
+
+    # -------------------------- Exit Message and Close -------------------------- #
+    print("Your map is complete! Open map.jpg to see your finished result!\n")
     output_jgr.close()
 
 if __name__ == "__main__":
